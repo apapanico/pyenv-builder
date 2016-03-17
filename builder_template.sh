54e493e9ca52b49fc309c9eddef9bdd70f4bdaf5
Alex Papanicolaou <alex.papanicolaou@integral.com>
2016-03-16 18:11:55 -0700

    init commit

---
+++ builder_template.sh
@@ -0,0 +1,29 @@
+
+LOCATION={location}
+
+pyenv uninstall -f {py_env}
+pyenv virtualenv --no-wheel {py_version} {py_env}
+pyenv rehash
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+
+
+echo 'Setting pyenv local to {py_env}'
+pyenv local {py_env}
+
+pyenv activate {py_env}
+pyenv which pip
+
+echo 'Installing {requirement_list}'
+pip install --no-binary :all: {requirement_list}
+
+for pkg in {lib_list}
+do
+	echo Installing $pkg
+    LIB_LOC=$LOCATION/lib/$pkg
+    (cd $LIB_LOC && python setup.py install)
+done
+
+# pyenv deactivate
---
+++ pyenv-builder.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python
+
+import os
+import yaml
+from subprocess import call
+import argparse
+
+BASE_DIR = '/home/alexp/Projects/pyenv-builder/'
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    'spec', help='Specification for pyenv virtual environment.')
+args = parser.parse_args()
+specfile = os.path.abspath(args.spec)
+location = os.path.dirname(specfile)
+
+spec = yaml.load(file(specfile, 'r').read())
+spec['location'] = location
+spec['py_env'] = '-'.join(
+    [spec['package'], spec['version'], 'py' + spec['py_version']])
+spec['requirement_list'] = ' '.join(spec['requirements'])
+spec['lib_list'] = ' '.join(spec['libs'])
+
+builder_script = file(
+    os.path.join(BASE_DIR, 'builder_template.sh'), 'r').read().format(**spec)
+
+p = call(builder_script, shell=True)
